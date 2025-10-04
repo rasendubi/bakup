333f82998c628df912351c2a3fd5f6541e89bd90
Maks Sydorenko <max.s@fluxonapps.com>
2025-10-04 17:35:39 +0200

    Path processing added
    Co-authored-by: Oleksii Shmalko <rasen.dubi@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
-use bakup::hello_world;
+use std::path::{Path, PathBuf};
+
+fn process_path(path: &Path, out_dir: &Path) -> Result<(), std::io::Error> {
+    let metadata = path.symlink_metadata()?;
+
+    if metadata.is_file() {
+        let content = std::fs::read(path)?;
+        let hash = blake3::hash(&content);
+
+        let out_path = out_dir.join(&hash.to_string());
+        if out_path.exists() {
+            println!("Skipping non-modified file: {path:?}");
+            return Ok(());
+        }
+
+        std::fs::write(&out_path, content)?;
+    } else if metadata.is_dir() {
+        for entry in path.read_dir()? {
+            let entry = entry?;
+            process_path(&entry.path(), out_dir)?;
+        }
+    }
+
+    Ok(())
+}
 
 fn main() {
-    hello_world();
+    let mut args = std::env::args_os();
+    let path = PathBuf::from(args.nth(1).expect("No path provided"));
+    let out_dir = PathBuf::from(args.next().expect("No output directory provided"));
+
+    std::fs::create_dir_all(&out_dir).expect("Failed to create output directory");
+    process_path(&path, &out_dir).expect("Failed to process path");
 }
