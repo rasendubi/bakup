6cedee3a1203f695c30969f54fe8a7b39ae9717f
Oleksii Shmalko <rasen.dubi@gmail.com>
2025-10-04 10:00:13 +0300

    Initialize rust template project

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "bakup";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.rustup
+          ];
+        };
+      });
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use bakup::hello_world;
+
+fn main() {
+    hello_world();
+}
